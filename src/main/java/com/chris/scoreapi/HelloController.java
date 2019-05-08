f34be74647c5d2ba583521e17f328f0d536f004f
Christophe Bortier <christophe.bortier@gmail.com>
2019-05-08 21:23:11 +0200

    added functionnality, implemented in HelloController

--- src/main/java/com/chris/scoreapi/HelloController.java
+++ src/main/java/com/chris/scoreapi/HelloController.java
@@ -1,5 +1,6 @@
 package com.chris.scoreapi;
 
+import com.chris.scoreapi.common.Translator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World, this is ScoreAPI";
+        return Translator.toLocale("hello");
     }
 }
